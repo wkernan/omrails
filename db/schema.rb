4fea65981d8e941e1fa8fc3f608b85c24f495966
Bill Kernan <wkernan@gmail.com>
2013-03-01 15:12:13 -0600

    fixed schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(:version => 20130228013503) do
     t.datetime "image_updated_at"
   end
 
-  add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
+  add_index "pins", ["user_id"], :username => "index_pins_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -39,11 +39,10 @@ ActiveRecord::Schema.define(:version => 20130228013503) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"
     t.string   "username"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], :username => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :username => "index_users_on_reset_password_token", :unique => true
 
 end
